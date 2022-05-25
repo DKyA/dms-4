1ea3b0201c555152cbd53617cd9a3a4151b9acba
Daniel Kyndl (Mac) <daniel.kyndl2@seznam.cz>
2022-05-25 18:06:24 +0200

    Oprava chyby ve třídě formuláře

--- admin/components/modules/_form.php
+++ admin/components/modules/_form.php
@@ -9,7 +9,7 @@ $nested => další komponenty
 */
 ?>
 
-<form action="<?=$_SERVER['PHP_SELF']?>" <?=$attributes['method']?> class="c-form c-form--<?=$attributes['class']?>" id='<?=$id?>'>
+<form action="<?=$_SERVER['PHP_SELF']?>" <?=$attributes['method']?> class="c-form c-form<?=$attributes['class']?>" id='<?=$id?>'>
 	<h1 class="c-form__headline"><?=$data[0]?></h1>
 	<div class="c-form__body">
 	<?php
