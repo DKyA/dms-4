c392d7b0fdf8a7c0c4e8dba22710d7a31fa5fcbe
Daniel Kyndl (Mac) <daniel.kyndl2@seznam.cz>
2022-06-05 23:45:14 +0200

    Doplnění atributů accordionu

--- admin/components/modules/_accordion.php
+++ admin/components/modules/_accordion.php
@@ -7,18 +7,28 @@ $attributes => atributy do HTML tagů (asociativní pole)
 $nested => další komponenty
 
 */
+
 ?>
 
-<section class="c-accordion">
+<section class="c-accordion c-accordion<?=$attributes['class']?>" accordion-selector>
+    <div class="c-accordion__self">
     <div class="c-accordion__headbar">
-        <h1><?=$data[0]?></h1>
-        <a <?=$attributes['link']?>>V</a>
-        <button class="c-accordion__control" accordion-toggle>X</button>
+        <legend class="c-accordion__legend">
+            <a <?=$attributes['link']?> class="c-accordion__link" <?=$attributes['title']?> target="_blank">
+                <img src="/admin/static/icons/link-45deg.svg" alt="Link na komponentu">
+            </a>
+            <h3><?=$data[0]?></h3>
+        </legend>
+        <button class="c-accordion__control" accordion-toggle title="Rozbalit obsah">
+            <img src="/admin/static/icons/chevron-down.svg" alt="Rozbalit accordion">
+        </button>
     </div>
     <div class="c-accordion__body">
         <?php
             apply_module($nested);
         ?>
+    </div>
+
     </div>
 </section>
 
