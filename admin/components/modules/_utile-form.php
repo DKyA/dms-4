b761a01118a6644a1cf1e626a088b1ba93799726
Daniel Kyndl (Mac) <daniel.kyndl2@seznam.cz>
2022-06-08 22:42:43 +0200

    Příprava na kódování utile formy

--- admin/components/modules/_utile-form.php
+++ admin/components/modules/_utile-form.php
@@ -11,10 +11,10 @@ $nested => další komponenty
 
 <form action="<?=$_SERVER['PHP_SELF']?>" <?=$attributes['method']?> class="c-u-form c-u-form<?=$attributes['class']?>" id='<?=$id?>'>
 
-	<div class="c-form__s-controllers">
-		<button class="c-form__button c-form__button--danger" type="submit" role="submit" title="" aria-label="">X</button>
-		<button class="c-form__button c-form__button--warning" type="submit" role="submit" title="" aria-label="">X</button>
-		<button class="c-form__button c-form__button--green" type="submit" role="submit" title="" aria-label="">X</button>
+	<div class="c-u-form__s-controllers">
+		<button class="c-u-form__button c-u-form__button--danger" type="submit" role="submit" title="" aria-label="">X</button>
+		<button class="c-u-form__button c-u-form__button--warning" type="submit" role="submit" title="" aria-label="">X</button>
+		<button class="c-u-form__button c-u-form__button--green" type="submit" role="submit" title="" aria-label="">X</button>
 	</div>
 
 	<?php
