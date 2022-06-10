e13e5e190cc15a68c67ed537670222cb9b455e49
Daniel Kyndl (Mac) <daniel.kyndl2@seznam.cz>
2022-06-10 21:00:42 +0200

    Úprava HTML pro utile-form

--- admin/components/modules/_utile-form.php
+++ admin/components/modules/_utile-form.php
@@ -12,13 +12,16 @@ $nested => další komponenty
 <form action="<?=$_SERVER['PHP_SELF']?>" <?=$attributes['method']?> class="c-u-form c-u-form<?=$attributes['class']?>" id='<?=$id?>'>
 
 	<div class="c-u-form__s-controllers">
-		<button class="c-u-form__button c-u-form__button--danger" type="submit" role="submit" title="" aria-label="">X</button>
-		<button class="c-u-form__button c-u-form__button--warning" type="submit" role="submit" title="" aria-label="">X</button>
-		<button class="c-u-form__button c-u-form__button--green" type="submit" role="submit" title="" aria-label="">X</button>
+		<button class="c-u-form__button c-u-form__button--danger" type="button" role="button" title="Odstranit prvek... (Otevře rozcestník)" aria-label="Odstranit prvek nastavení (otevře rozcestník)"></button>
+		<button class="c-u-form__button c-u-form__button--warning" type="submit" role="submit" title="Uložit" aria-label="Uloží hodnoty nastavení"></button>
+		<button class="c-u-form__button c-u-form__button--green" type="button" role="button" title="Přidat prvek... (Otevře rozcestník)" aria-label="Přidat prvek nastavení... (Otevře rozcestník)"></button>
 	</div>
 
-	<?php
-		apply_module($nested);
-	?>
+    <div class="c-u-form__inputs">
+        <?php
+            apply_module($nested);
+        ?>
+    </div>
+
 
 </form>
